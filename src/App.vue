85535fb05229615bb15cb2fd0c0afe34aa73343e
Shaun Kiszonas <shadoewraith99@gmail.com>
2022-10-22 22:43:21 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+  name: 'app',
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style>
+@import url('./style.css');
+
+body {
+  background-color: #333;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="flex bg-transparent text-white py-4 border-b border-slate-2300">
+    <h1 class="mr-auto ml-auto text-2xl">Portfolio++</h1>
+    <ul class="flex text-md gap-4 items-center ml-auto mr-auto">
+      <li class="nav-item"><a href="/">Home</a></li>
+      <li class="nav-item"><a href="/network">Network</a></li>
+      <li class="nav-item"><a href="/profile">Profile</a></li>
+      <li class="nav-item"><a href="/logout">Logout</a></li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+@import url('../style.css');
+@layer components {
+  .nav-item {
+    @apply hover:text-sky-400 cursor-pointer rounded-md px-1 duration-200;
+  }
+}
+</style>
